238d741bcf9d8eb47b87cf67bea59ad6d8af8476
LAPTOP-U0MO5ODO\shake <shaketshah90@gmail.com>
2020-08-16 19:19:54 +0530

    Redirect to Login if Page not found or Default URL

--- src/routing/RoutePath.js
+++ src/routing/RoutePath.js
@@ -11,12 +11,20 @@ import ApiCall from '../integration/ApiCall';
 const RoutePath = () => {
     return (
         <Router>
+            <Switch>
             <Route path="/login" component={Login} ></Route>
             <Route path="/create-account" component={CreateAccount} ></Route>
             <Route path="/users" component={Users} ></Route>
             <Route path="/feeds" component={Feeds} ></Route>
             <Route path="/profile" component={Profile} ></Route>
             <Route path="/apicall" component={ApiCall} ></Route>
+            <Route exact path="/">
+                <Redirect to="/login" />
+            </Route>
+            <Route path="*">
+                <Redirect to="/login" />
+            </Route>
+            </Switch>
         </Router>
     )
 }
